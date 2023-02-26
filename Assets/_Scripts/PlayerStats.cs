5910e2f2bc9f2ba4914377e0a422e0370f034b0b
André Costa <77451181+andrefnc4@users.noreply.github.com>
2023-02-26 19:27:20 +0000

    Photon Fusion imported

---
+++ Assets/_Scripts/PlayerStats.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+using UnityRandom = UnityEngine.Random;
+
+
+public class PlayerStats : MonoBehaviour, IHitable
+{
+    //General Properties
+    [Header("General Properties")]
+    [SerializeField] float _maxHP;
+    [SerializeField] bool _isPushable;
+    float _currentHp;
+
+    //ReadOnly Variables
+    public float MaxHP => _maxHP;
+    public float CurrentHp => _currentHp;
+
+    //Temporary Shit
+    public TextMeshProUGUI hpText;
+
+    private void Start()
+    {
+        _currentHp = MaxHP;
+    }
+
+
+    private void Update()
+    {
+        hpText.text = _currentHp.ToString() + " / " + MaxHP.ToString();
+    }
+
+
+
+    public void HandleHit(Damage damage)
+    {
+        _currentHp -= damage._amount;
+        if (_currentHp <= 0)
+        {
+            _currentHp = 0;
+            Invoke("ResetHealth", 1.5f);
+        }
+        
+        
+        
+    }
+
+    //TEST ONLY 
+    public void ResetHealth()
+    {
+        _currentHp = MaxHP;
+    }
+
+    public IEnumerator DealDamageEnum(Damage damage, Vector3 forceDirection)
+    {
+        float damagePerTick = damage._amount / damage._tickAmount;
+        float delayBetweenTicks = damage._damageOverTimeDuration / damage._tickAmount;
+
+        yield return null;
+    }
+
+
+
+}
--- Assets/_Scripts/TempUIInfo.cs
+++ Assets/_Scripts/TempUIInfo.cs
@@ -11,14 +11,14 @@ public class TempUIInfo : MonoBehaviour
     [SerializeField] RawImage _weaponImage;
     [SerializeField] RawImage _offWeaponImage;
     private PlayerCombatHandler _playerCombatHandler;
-    private Stats _playerStats;
+    private PlayerStats _playerStats;
     private GunSO _lastFrameWeapon;
     public Animator _weaponUIAnimator;
     // Start is called before the first frame update
     void Start()
     {
         _playerCombatHandler = _player.GetComponent<PlayerCombatHandler>();
-        _playerStats = _player.GetComponent<Stats>();
+        _playerStats = _player.GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame

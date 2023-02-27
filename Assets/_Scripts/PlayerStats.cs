fe561cd55fc05bc28cfd088251ccdd7336c4f459
ruic14 <76440654+ruic14@users.noreply.github.com>
2023-02-27 17:18:51 +0000

    health and shield
    pequena amostra do sistema d vida e shield no inimigo

--- Assets/_Scripts/PlayerStats.cs
+++ Assets/_Scripts/PlayerStats.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using TMPro;
 using UnityRandom = UnityEngine.Random;
+using UnityEngine.UI;
 
 
 public class PlayerStats : MonoBehaviour, IHitable
@@ -12,12 +13,16 @@ public class PlayerStats : MonoBehaviour, IHitable
     //General Properties
     [Header("General Properties")]
     [SerializeField] float _maxHP;
+    [SerializeField] float _maxShield;
     [SerializeField] bool _isPushable;
     float _currentHp;
+    public Slider healthBar;
+    public Slider shieldBar;
 
     //ReadOnly Variables
     public float MaxHP => _maxHP;
     public float CurrentHp => _currentHp;
+    public float _shield;
 
     //Temporary Shit
     public TextMeshProUGUI hpText;
@@ -25,11 +30,16 @@ public class PlayerStats : MonoBehaviour, IHitable
     private void Start()
     {
         _currentHp = MaxHP;
+        _shield = 25;
+        _maxShield = 60;
+
     }
 
 
     private void Update()
     {
+        shieldBar.value = _shield;
+        healthBar.value = _currentHp;
         hpText.text = _currentHp.ToString() + " / " + MaxHP.ToString();
     }
 
@@ -37,7 +47,15 @@ public class PlayerStats : MonoBehaviour, IHitable
 
     public void HandleHit(Damage damage)
     {
-        _currentHp -= damage._amount;
+        if (_shield > 0)
+        {
+            _shield -= damage._amount;
+        }
+        else
+        {
+            _currentHp -= damage._amount;
+        }
+      //  _currentHp -= damage._amount;
         if (_currentHp <= 0)
         {
             _currentHp = 0;

9ac2eb280bd74672edbc628ef3e45cc06340b117
ruic14 <76440654+ruic14@users.noreply.github.com>
2023-03-23 18:49:39 +0000

    update mini game

--- Assets/_Scripts/miniGame_manager.cs
+++ Assets/_Scripts/miniGame_manager.cs
@@ -9,6 +9,7 @@ public class miniGame_manager : MonoBehaviour
 
     bool game_started = false;
     public GameObject player;
+    public GameObject camera;
 
 
     void Start()
@@ -20,12 +21,37 @@ public class miniGame_manager : MonoBehaviour
     void Update()
     {
         if (running){
-            player.transform.position -= new Vector3(0, 0, 0.5f * Time.deltaTime);
+            player.transform.position -= new Vector3(0.5f * Time.deltaTime, 0, 0);
+            camera.transform.position -= new Vector3(0.5f * Time.deltaTime, 0, 0 );
         }
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !game_started) {
             running = true;
+            game_started = true;
+            StartCoroutine(Sing());
         }
 
+
+
+    }
+
+
+
+    IEnumerator Sing()
+    {
+        //audio play
+        yield return new WaitForSeconds(4.5f);
+        //animator -> look
+        yield return new WaitForSeconds(2);
+        //check player move
+        if (running){
+            Debug.Log("shoot");
+        }
+
+        yield return new WaitForSeconds(2);
+        //idle
+        yield return new WaitForSeconds(1);
+        //audio stop
+        StartCoroutine(Sing());
 
 
     }

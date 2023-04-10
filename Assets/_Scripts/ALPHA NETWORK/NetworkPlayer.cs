8f6ad3768562617c257d3b809acfc8461749a0db
André Costa <77451181+andrefnc4@users.noreply.github.com>
2023-04-10 15:04:13 +0100

    fix camera, respawn function

--- Assets/_Scripts/ALPHA NETWORK/NetworkPlayer.cs
+++ Assets/_Scripts/ALPHA NETWORK/NetworkPlayer.cs
@@ -19,9 +19,21 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
         {
             Local = this;
 
+            //disable main camera
+            Camera.main.gameObject.SetActive(false);
+
             Debug.Log("Spawned local player");
         }
-        else Debug.Log("Spawned remote player");
+        else {
+            // disable the camera if we are not the local player
+            Camera localCamera = GetComponentInChildren<Camera>();
+            localCamera.enabled = false;
+
+            AudioListener audioListener = GetComponentInChildren<AudioListener>();
+            audioListener.enabled = false;
+
+            Debug.Log("Spawned remote player"); 
+        }
     }
 
     public void PlayerLeft(PlayerRef player)
